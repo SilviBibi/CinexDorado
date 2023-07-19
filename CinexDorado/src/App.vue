fa6ec7b828a81bae2b6c284af201e96b7432a28d
Silvia Falcon <silvi09_24@hotmail.com>
2023-07-18 21:42:10 -0500

    Instalando dependencias necesarias e iniciando proyecto

---
+++ CinexDorado/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+
+import Magnify from 'vue-material-design-icons/Magnify.vue';
+import HomeOutLine from 'vue-material-design-icons/HomeOutLine.vue';
+import TrendingUp from 'vue-material-design-icons/TrendingUp.vue';
+import MovieOutLine from 'vue-material-design-icons/MovieOutLine.vue';
+import Plus from 'vue-material-design-icons/Plus.vue';
+import ChevronLeft from 'vue-material-design-icons/ChevronLeft.vue';
+
+import {useMovieStore} from './stores/movie'
+import {storeToRefs} from 'pinia';
+
+const useMovie= useMovieStore();
+const {movie, showFullVideo} = storeToRefs(useMovie)
+
+</script>
+
+<template>
+<div class="fixed w-full h-screen bg-black">
+  <div v-if="!showFullVideo" id="SideNav" class="fixed z-40 items-center w-[120px] h-screen bg-black relative">
+    IMAGE
+  </div>
+</div>
+</template>
